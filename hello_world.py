29ca0fa7ac1ad5374711c5686d1e6a2869a3196b
Lisan <lisa.lassi@live.se>
2025-10-30 16:07:07 +0100

    Add hello_world.py with initial print statements and a NameError example

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+#print("Hello Python world!")
+
+message = "Hello Python World!" # message = variable
+print(message)
+
+message = "Hello Python Crash Course world!"
+print(message)
+
+message = "Hello Python Crash Course reader!"
+print(mesage) #Gives a NameError because name "mesage" is not defined.
