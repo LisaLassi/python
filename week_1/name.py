5008af43178fb7d9ba6b8c84c0bb54ca90ce5f5c
Lisan <lisa.lassi@live.se>
2025-10-30 16:38:03 +0100

    Add name.py with greeting message and programming languages list

---
+++ week_1/name.py
@@ -0,0 +1,13 @@
+#name = "lisa lassi"
+#print(name.title()) # .title() method capitalizes the first letter of each word
+#print(name.upper())
+#print(name.lower())
+
+first_name = "lisa"
+last_name = "lassi"
+full_name = f"{first_name} {last_name}"
+
+message = f"Hello, {full_name.title()}!"
+print(message)
+
+print("Languages: \n\tPython\n\tC#\n\tJavaScript") # \n for new line, \t for tab
