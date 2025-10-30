988b329b385adf22ec00a9be7ae22ee5e66fba78
Lisan <lisa.lassi@live.se>
2025-10-30 16:37:51 +0100

    Reintroduce hello_world.py with example messages and NameError demonstration

---
+++ week_1/hello_world.py
@@ -0,0 +1,23 @@
+"""HELLO WORLD"""
+
+#print("Hello Python world!")
+
+#message = "Hello Python World!" # message = variable
+#print(message)
+
+#message = "Hello Python Crash Course world!"
+#print(message)
+
+#message = "Hello Python Crash Course reader!"
+#print(mesage) #Gives a NameError because name "mesage" is not defined.
+
+# 2-1 Simple message
+message = "Hello my name is Lisa"
+print(message)
+
+# 2-2 Simple messages
+message2 = "I am 27 years old"
+print(message2)
+
+message2 = "I like coffe"
+print(message2)
